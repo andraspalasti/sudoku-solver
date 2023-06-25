863acf3251d87d51cae07686d1b0f2433f5a687d
andraspalasti <andraspalasti2@gmail.com>
2023-06-25 13:58:05 +0200

    restructured project

---
+++ src/train_localizer.py
@@ -0,0 +1,178 @@
+import argparse
+import os
+import shutil
+
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import ConcatDataset, DataLoader, random_split
+from tqdm import tqdm
+
+from data.datasets import RandomImageDataset, PuzzleDataset
+from models import Localizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--device', default='cpu',
+                    help='the device that should perform the computations')
+parser.add_argument('--epochs', default=50, type=int, metavar='N', choices=range(1000),
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int, choices=[2 ** x for x in range(1, 9)],
+                    metavar='N',
+                    help='mini-batch size (default: 256), this is the total '
+                         'batch size of all GPUs on the current node when '
+                         'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--lr', '--learning-rate', default=0.001, type=float,
+                    metavar='LR', help='initial learning rate', dest='learning_rate')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+
+
+def train(training_loader, model, criterion, optimizer, epoch, device):
+
+    #  Switch to training mode
+    model.train()
+
+    progress = tqdm(training_loader, desc=f'EPOCH {epoch}')
+
+    running_loss = 0.0
+    for i, (images, targets) in enumerate(progress):
+
+        # move data to the same device as model
+        images = images.to(device, non_blocking=True)
+        targets = targets.to(device, non_blocking=True)
+
+        outputs = model(images)
+        loss = criterion(outputs, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        progress.set_postfix({ 'AvgLoss': running_loss / (i+1) })
+
+    return running_loss / (i+1)
+
+
+def validate(val_loader, model, criterion, device):
+        model.eval()
+        running_vloss = 0.0
+        with torch.no_grad():
+            for i, (inputs, targets) in enumerate(val_loader):
+                # move data to the same device as model
+                inputs = inputs.to(device, non_blocking=True)
+                targets = targets.to(device, non_blocking=True)
+
+                voutputs = model(inputs)
+                vloss = criterion(voutputs, targets)
+                running_vloss += vloss
+        return running_vloss / (i+1)
+
+
+def main():
+    args = parser.parse_args()
+
+    #  Loss function
+    def criterion(outputs, targets):
+        # the classification has a big impact too
+        loss = F.binary_cross_entropy(outputs[0], targets[:, :2]) * 1000
+        loss += F.mse_loss(outputs[1], targets[:, 2:])
+        return loss
+
+    device = torch.device(args.device)
+
+    model = Localizer()
+    model.to(device)
+
+    dataset = ConcatDataset([
+        *([RandomImageDataset(
+            'data/images',
+            transform=transforms.Compose([
+                transforms.Grayscale(),
+                transforms.RandomCrop((224, 224)),
+                transforms.ToTensor(),
+                lambda img: torch.cat((img, img, img))
+            ]),
+            target_transform=lambda bbox: torch.tensor(
+                [0, 1, *bbox], dtype=torch.float32),
+        )] * 10),
+        PuzzleDataset(
+            csv_file='data/outlines_sorted.csv',
+            root_dir='data',
+            transform=transforms.Compose([
+                transforms.Grayscale(),
+                transforms.ToTensor(),
+                lambda img: torch.cat((img, img, img))
+            ]),
+            target_transform=lambda bbox: torch.tensor(
+                [1, 0, *bbox], dtype=torch.float32),
+        ),
+    ])
+
+    train_size = int(len(dataset) * 0.9)
+    validation_size = len(dataset) - train_size
+    train_set, validation_set = random_split(dataset, [train_size, validation_size])
+
+    training_loader = DataLoader(
+        train_set, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+    validation_loader = DataLoader(
+        validation_set, batch_size=args.batch_size, shuffle=False, pin_memory=True)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=0.1)
+
+    # Sets the learning rate to the initial LR decayed by 10 every 200 epochs
+    scheduler = StepLR(optimizer, step_size=200, gamma=0.1)
+    best_loss = 1 << 32
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print(f"=> loading checkpoint '{args.resume}'")
+            checkpoint = torch.load(args.resume, map_location=device)
+
+            args.start_epoch = checkpoint['epoch']
+            best_loss = checkpoint['best_loss']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            scheduler.load_state_dict(checkpoint['scheduler'])
+
+            print(f"=> loaded checkpoint '{args.resume}' (epoch {checkpoint['epoch']})")
+        else:
+            print(f"=> no checkpoint found at: '{args.resume}'")
+
+    if args.evaluate:
+        avg_vloss = validate(validation_loader, model, criterion, device)
+        print(f'avarage validation loss: {avg_vloss}')
+        return
+
+    for epoch in range(args.start_epoch, args.epochs):
+        avg_loss = train(training_loader, model, criterion, optimizer, epoch, device)
+
+        avg_vloss = validate(validation_loader, model, criterion, device)
+        print(f'LOSS train {avg_loss} valid {avg_vloss}')
+
+        is_best = best_loss > avg_vloss
+        best_loss = max(best_loss, avg_vloss)
+
+        scheduler.step()
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'best_loss': best_loss,
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict()
+        }, is_best)
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/webcam_localizer.py
@@ -0,0 +1,61 @@
+import cv2
+import torch
+
+from models import Localizer
+
+
+def main():
+    device = torch.device('cpu')
+
+    model = Localizer().to(device)
+    model.eval()
+
+    # Load weights
+    checkpoint = torch.load('models/model_best.pth.tar', map_location=device)
+    model.load_state_dict(checkpoint['state_dict'])
+
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(1)
+
+    if vc.isOpened():  # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        image = cv2.GaussianBlur(image, (5, 5), 0)
+        topy, topx = (image.shape[0] - 500) // 2, (image.shape[1] - 500) // 2
+        image = image[topy:topy+500, topx:topx+500]
+        scaley, scalex = image.shape[0] / 224, image.shape[1] / 224 
+        image = cv2.resize(image, (224, 224))
+
+        input = torch.tensor(image, dtype=torch.float32, device=device).unsqueeze(dim=0)
+        input = torch.cat((input, input, input))
+        input = input / 255
+
+        probs, bbox = model(input.unsqueeze(dim=0))
+        bbox[0, ::2] = bbox[0, ::2] * scalex
+        bbox[0, 1::2] = bbox[0, 1::2] * scaley
+        bbox = bbox[0].tolist()
+        present_prob = round(probs[0, 0].item() * 100)
+        x1, y1 = int(topx + bbox[0]), int(topy + bbox[1])
+        x2, y2 = int(topx + bbox[2]), int(topy + bbox[3])
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=3)
+        frame = cv2.rectangle(frame, (topx, topy), (topx+500, topy+500), color=(0, 255, 0), thickness=3)
+        frame = cv2.putText(frame, f'{present_prob}%', org=(x1+10, y1+20),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5, thickness=2, color=(255, 0, 0))
+
+        key = cv2.waitKey(20)
+        if key == 27:  # exit on ESC
+            break
+
+    vc.release()
+    cv2.destroyWindow("preview")
+
+if __name__ == '__main__':
+    main()
