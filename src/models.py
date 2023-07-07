1f72e1118a9fe7677a6ec9f80850c2c7f59ad7e6
andraspalasti <andraspalasti2@gmail.com>
2023-07-07 15:23:51 +0200

    created digit classifier model

--- src/models.py
+++ src/models.py
@@ -49,25 +49,80 @@ class Localizer(nn.Module):
         x = self.mobilenet.features(x)
 
         x = nn.functional.adaptive_avg_pool2d(x, (1, 1))
-        x = torch.flatten(x, 1)
+        x = torch.flatten(x, start_dim=1)
 
         classification = nn.functional.sigmoid(self.mobilenet.classifier(x))
         localization = self.localization(x)
         return classification, localization
 
 
+class DigitClassifier(nn.Module):
+    """Classifier for handwritten digits."""
+
+    def __init__(self):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.ReLU(inplace=True),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(16, 120, kernel_size=5),
+            nn.ReLU(inplace=True),
+        )
+
+        n_hidden = 84
+        self.classifier = nn.Sequential(
+            nn.Linear(120, n_hidden),
+            nn.ReLU(inplace=True),
+            nn.Linear(n_hidden, 10)
+        )
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out")
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x: torch.Tensor):
+        x = self.layers(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self.classifier(x)
+        return nn.functional.log_softmax(x, dim=1)
+
+
 if __name__ == '__main__':
     input = torch.randn((1, 1, 224, 224))
-
     model = Localizer()
     model.eval()
 
     num_params = sum(param.numel() for param in model.parameters())
+    print('Localizer: ')
     print(f'params: {num_params}')
-
     with torch.no_grad():
         start = time.perf_counter_ns()
         is_present, location = model(input)
         end = time.perf_counter_ns()
     print(f'is_present: {is_present.shape} location: {location.shape}')
+    print(f'{( end - start ) * 1e-6}ms\n')
+
+
+    input = torch.randn((1, 1, 28, 28))
+    model = DigitClassifier()
+    model.eval()
+
+    num_params = sum(param.numel() for param in model.parameters())
+    print('DigitClassifier: ')
+    print(f'params: {num_params}')
+    with torch.no_grad():
+        start = time.perf_counter_ns()
+        classification = model(input)
+        end = time.perf_counter_ns()
+    print(f'classification: {classification.shape}')
     print(f'{( end - start ) * 1e-6}ms')
