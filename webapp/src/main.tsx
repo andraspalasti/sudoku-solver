8fb688443016fdfb8933f697460f0b35eff2a1d4
andraspalasti <andraspalasti2@gmail.com>
2023-07-16 18:12:52 +0200

    fixed most problems

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import * as ort from 'onnxruntime-web';
 import './index.css';
 
+ort.env.wasm.wasmPaths = {
+  'ort-wasm-simd.wasm': 'sudoku-solver/ort-wasm-simd.wasm',
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -22,39 +22,40 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
-        "name": "Sudoku Solver",
-        "short_name": "Sudoku Solver",
-        "description": "This sudoku app lets you scan a sudoku than solve it.",
-        "theme_color": "#ffffff",
-        "icons": [
+        name: 'Sudoku Solver',
+        short_name: 'Sudoku Solver',
+        description: 'This sudoku app lets you scan a sudoku than solve it.',
+        theme_color: '#ffffff',
+        icons: [
           {
-            "src": "/pwa-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
           },
           {
-            "src": "/pwa-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "any"
+           src: 'pwa-512x512.png',
+           sizes: '512x512',
+           type: 'image/png',
+           purpose: 'any'
           },
           {
-            "src": "/apple-touch-icon.png",
-            "sizes": "180x180",
-            "type": "image/png",
-            "purpose": "any"
+            src: 'apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
           },
           {
-            "src": "/maskable_icon.png",
-            "sizes": "225x225",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: 'maskable_icon.png',
+            sizes: '225x225',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ],
-        "background_color": "#e8ebf2",
-        "display": "standalone",
-        "orientation": "portrait"
+        start_url: '/sudoku-solver/',
+        background_color: '#e8ebf2',
+        display: 'standalone',
+        orientation: 'portrait'
       }
     })
   ],
